723d40e857d1a3f9e9ff0cf2b255e335a06d07cd
Petr Zigel <zepeter@yandex.ru>
2013-03-29 18:58:35 +0400

    ajax table

---
+++ app/controllers/AjaxController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pzigel
+ * Date: 29.03.13
+ * Time: 11:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class AjaxController {
+}
